1e969cfc8a701301a008ad067c6e3140fdbd110e
Worranan <nun.45_w@hotmail.com>
2021-10-24 22:30:10 +0700

    Why I can not click on button multiple times????

--- Untitled-11.py
+++ Untitled-11.py
@@ -1,28 +1,32 @@
+from PyQt5.QtGui import QFont
 from PyQt5.QtWidgets import *
 
 class MyWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Calculator")
-
+        self.resize(1000,1000)
         self.c = ""
 
-        self.labelResult = QLabel()
-        self.setGeometry(0,0,125*9,50)
+        self.labelResult = QLabel(self)
+        self.labelResult.setGeometry(25,25,125*9,100)
+        self.labelResult.setFont(QFont("Times", 30))
 
+        
         g = 10
-        # for i in range(0,10):
-        #     self.button = QPushButton("{}".format(i),self)
-        #     self.button.setGeometry(125*i,100,100,100)
         for i in range(0,3):
             for i2 in range(0,3):
                 g -= 1
                 self.button = QPushButton("{}".format(g),self)
-                self.button.setGeometry(125*i2,130+125*i,100,100)
+                self.button.setGeometry(25+(125*i2),130+(125*i),100,100)
+                self.button.setFont(QFont("Times",30))
 
-        #     self.button.clicked.connect(self.but)
-        # def but(self):
-        
+                self.button.clicked.connect(lambda checked, g = g: self.but(g))
+
+    def but(self,i):
+        self.c += str(i)
+        self.labelResult.setText(self.c)
+            
             
         
         widget = QWidget()
